b11cbb8e4e38f1e492261f03ad940f5193737d2e
Ilia Nemankov <INemankov@luxoft.com>
2021-06-22 19:16:51 +0300

    Update client action (Apex Legends)

--- jobs/Scripts/clientTests.py
+++ jobs/Scripts/clientTests.py
@@ -140,23 +140,15 @@ def do_test_actions(game_name):
 
                 pydirectinput.keyDown("a")
                 pydirectinput.keyDown("space")
-                pyautogui.move(1000, 0, 0.33)
-                pydirectinput.move(1000, None)
                 sleep(0.5)
                 pydirectinput.keyUp("a")
                 pydirectinput.keyUp("space")
-                pyautogui.move(1000, 0, 0.33)
-                pydirectinput.move(1000, None)
 
                 pydirectinput.keyDown("d")
                 pydirectinput.keyDown("space")
-                pyautogui.move(1000, 0, 0.33)
-                pydirectinput.move(1000, None)
                 sleep(0.5)
                 pydirectinput.keyUp("d")
                 pydirectinput.keyUp("space")
-                pyautogui.move(1000, 0, 0.33)
-                pydirectinput.move(1000, None)
 
     except Exception as e:
         main_logger.error("Failed to do test actions: {}".format(str(e)))
